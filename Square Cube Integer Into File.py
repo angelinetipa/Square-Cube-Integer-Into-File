1cf303c1f33529a12ca3cc400b58782c25ea7280
angelinetipa <angelinetipa@gmail.com>
2023-04-25 21:58:45 +0800

    create def and open file

--- Square Cube Integer Into File.py
+++ Square Cube Integer Into File.py
@@ -1,6 +1,9 @@
 # define function that create double and triple text
-# open integers.txt [read], double.txt [append], triple.txt [append]
+def create_double_triple_txt():
+    # open integers.txt [read], double.txt [append], triple.txt [append]
+    with open("integers.txt") as file_int, open("double.txt", "a") as file_sqr, open("triple.txt", "a") as file_cube:
 # for each line in integers.txt
 # if integer is even, # write to double.txt the square of integer
 # if integer is odd, #write to triple.txt the cube of integer
 # call the function/output
+create_double_triple_txt()
