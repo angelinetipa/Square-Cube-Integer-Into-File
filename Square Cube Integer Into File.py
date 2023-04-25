53032cd3c22f7a57404805360e2b145226225e13
angelinetipa <angelinetipa@gmail.com>
2023-04-25 22:02:34 +0800

    if for even integer

--- Square Cube Integer Into File.py
+++ Square Cube Integer Into File.py
@@ -2,8 +2,13 @@
 def create_double_triple_txt():
     # open integers.txt [read], double.txt [append], triple.txt [append]
     with open("integers.txt") as file_int, open("double.txt", "a") as file_sqr, open("triple.txt", "a") as file_cube:
-# for each line in integers.txt
-# if integer is even, # write to double.txt the square of integer
+        # for each line in integers.txt
+        for line in file_int:
+            integer = int(line)
+            # if integer is even, # write to double.txt the square of integer
+            if integer % 2 == 0:
+                square = integer ** 2
+                file_sqr.write(str(square) + "\n")
 # if integer is odd, #write to triple.txt the cube of integer
 # call the function/output
 create_double_triple_txt()
