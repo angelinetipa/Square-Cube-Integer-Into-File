764065620158534532c9ccb50dc1e2f607b160dc
angelinetipa <angelinetipa@gmail.com>
2023-04-25 21:54:44 +0800

    pseudocode

---
+++ Square Cube Integer Into File.py
@@ -0,0 +1,6 @@
+# define function that create double and triple text
+# open integers.txt [read], double.txt [append], triple.txt [append]
+# for each line in integers.txt
+# if integer is even, # write to double.txt the square of integer
+# if integer is odd, #write to triple.txt the cube of integer
+# call the function/output
